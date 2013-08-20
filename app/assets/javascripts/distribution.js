3293b111832d0823a4787500dc3e6eaa8799375d
Jeff Carpenter <gcarpenterv@gmail.com>
2013-08-19 20:54:12 -0700

    Added hover to cat distribution chart

--- app/assets/javascripts/distribution.js
+++ app/assets/javascripts/distribution.js
@@ -1,38 +1,42 @@
-if (document.getElementById('distribution-chart')){
+(function() {
+  if (document.getElementById('distribution-chart') && rcrd.frequencies) {
 
-  console.log(frequencies);
+    var data = rcrd.frequencies; // In tpl
+    var width = 600;
+    var height = 100;
+    var barWidth = width / data.length;
 
-  var data = frequencies; // In tpl
+    var x = d3.scale.linear()
+        .domain([0, data.length])
+        .range([0, width]);
 
-  console.log(600 / data.length);
+    var y = d3.scale.linear()
+        .domain([0, d3.max(data, function(d) { return d.times; })])
+        .range([0, height]);
 
-  var width = 600;
-  var height = 100;
+    var svg = d3.select("#distribution-chart").append("svg")
+        .attr("width", "100%")
+        .attr("height", height)
+        .attr("viewBox", "0 0 "+width+" 100")
+        .attr("preserveAspectRatio", "xMinYMin meet")
+      .append("g")
 
-  var barWidth = width / data.length;
+   svg.selectAll("rect")
+       .data(data)
+     .enter().append("rect")
+       .attr("x", function(d, i) { return x(i); })
+       .attr("y", function(d) { return height - y(d.times); })
+       .attr("width", barWidth)
+       .attr("height", function(d) { return y(d.times); })
+       .attr("fill", "steelblue")
+       .on("mouseover", function(d) {
+            d3.select(this).style("fill", "#5c93c0")
+            d3.select("#cat-name h2").html(d.name);
+       })
+       .on("mouseout", function(d) {
+            d3.select(this).style("fill", "steelblue");
+            d3.select("#cat-name h2").html(d.name);
+       })
 
-  var x = d3.scale.linear()
-      .domain([0, data.length])
-      .range([0, width]);
-
-  var y = d3.scale.linear()
-      .domain([0, d3.max(data, function(d) { return d.times; })])
-      .range([0, height]);
-
-  var svg = d3.select("#distribution-chart").append("svg")
-      .attr("width", "100%")
-      .attr("height", 400)
-      .attr("viewBox", "0 0 "+width+" 100")
-      .attr("preserveAspectRatio", "xMinYMin meet")
-    .append("g")
-
- svg.selectAll("rect")
-     .data(data)
-   .enter().append("rect")
-     .attr("x", function(d, i) { return x(i); })
-     .attr("y", function(d) { return height - y(d.times); })
-     .attr("width", barWidth)
-     .attr("height", function(d) { return y(d.times); })
-     .attr("fill", "steelblue")
-
-}
+  }
+})();
